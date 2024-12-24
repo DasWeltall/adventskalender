cf579d65ccef58185d8fa4b067f0ed663f56b823
DasWeltall <151219570+DasWeltall@users.noreply.github.com>
2024-12-24 08:50:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function openDoor(div, day) {
                 div.style.backgroundColor = 'rgb(204, 102, 255)';
                 break;
             case 24:
-                div.innerHTML = <a href="24.html">Open Present</a>;
+                div.innerHTML = '<a href="24.html">Open Present</a>';
                 div.style.backgroundColor = 'rgb(255, 255, 153)';
                 break;
             default:
